224b4255e4cedb4a7a4d8237563f363af6b30ae7
Jnmendza <jonatmendoza@gmail.com>
2020-06-11 00:19:34 -0700

    boilerplate new components and added details, cart, and default route

--- kit10store/src/App.js
+++ kit10store/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
 import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
 
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={ProductList}></Route>
+        <Route  path="/details" component={Details}></Route>
+        <Route  path="/cart" component={Cart}></Route>
+        <Route component={Default}></Route>
       </Switch>
     </div>
   );
---
+++ kit10store/src/components/Details.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Details = () => {
+    return (
+        <div>
+            <h1>This is details</h1>
+        </div>
+    )
+}
+
+export default Details
