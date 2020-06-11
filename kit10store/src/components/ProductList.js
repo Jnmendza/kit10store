ba7444c8b50972f11db236e71b6209d1b6500456
Jnmendza <jonatmendoza@gmail.com>
2020-06-11 00:11:24 -0700

    Navbar and home route

---
+++ kit10store/src/components/ProductList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ProductConsumer } from '../context'
+
+const ProductList = () => {
+    return (
+        <div>
+            <h1>This is product list</h1>
+        </div>
+    )
+}
+
+export default ProductList
